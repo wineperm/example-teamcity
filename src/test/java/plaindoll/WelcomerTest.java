fbf7cc00c76fe8cfd7f446e0bcf551cc879470d3
wineperm <leo_kraska@mail.ru>
2024-03-11 17:41:45 +0500

    Update WelcomerTest.java

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -31,7 +31,7 @@ public class WelcomerTest {
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
 	@Test
-	public void welcomerSaysPheasantHunter(){
+	public void welcomerSaysHunter(){
 		assertThat(welcomer.sayHunter(), containsString("hunter"));
 	}
 }
