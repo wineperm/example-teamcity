92e3a39428fdb22e466ea320abaaa6c409e1aeac
wineperm <leo_kraska@mail.ru>
2024-03-11 17:52:32 +0500

    Update Welcomer.java

--- src/main/java/plaindoll/Welcomer.java
+++ src/main/java/plaindoll/Welcomer.java
@@ -13,7 +13,7 @@ public class Welcomer{
 	public String saySome(){
 		return "something in the way";
 	}
-        public String sayHunter(){
+        public String sayHunterProverb(){
 		return "Run, you fools!";
-	}
+	}	
 }
